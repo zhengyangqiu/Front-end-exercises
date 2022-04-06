d1461893faba52c80b0358cac4d426dfa5a47668
zhengyang qiu <15224003720@163.com>
2022-04-06 08:44:11 +0100

    start react youtube

---
+++ week12/react-exercise/src/components/Greet.js
@@ -0,0 +1,17 @@
+//always import react
+import React from 'react';
+
+// function Greet(){
+//     return <h1>hello zhengyang</h1>
+// }
+
+//use array function is better than function
+
+const Greet = ()=><h1>hello zhengyang</h1>
+
+//export const Greet = () => <h1>hello zhengyang</h1> is name export, have to import component with exact same name and
+
+export default Greet;
+
+//it is allow import any name from app.js
+
